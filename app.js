3c71818b1878245a15cb5ee2c798c7a02d338c39
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-04-14 21:36:31 +0900

    set up2

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/api/v1/tasks", (req, res) => {
+  res.send("take task");
+});
+app.post("/api/v1/tasks", (req, res) => {
+  res.send("make new task");
+});
+
+app.get("/api/v1/tasks/:id", (req, res) => {
+  res.send("id task");
+});
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("change id task");
+});
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("delete id task");
+});
+
 app.listen(PORT, console.log("server on"));
