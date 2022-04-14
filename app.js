de911b3ba091927cb91980b2f5676a3767d07991
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-04-14 23:17:34 +0900

    db connect

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 const PORT = 3000;
-
+// root set up
 app.use("/api/v1/tasks", taskRoute);
-app.listen(PORT, console.log("server on"));
+
+//db connect
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log("server on"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
