fc053a6592818d94c8562b9f70b815f9a500cd41
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-04-15 12:49:39 +0900

    edit

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
 const taskDom = document.querySelector(".tasks");
 const formDom = document.querySelector(".task-form");
 const inputDom = document.querySelector(".task-input");
+const formAlertDom = document.querySelector(".form-alert");
 
 const showTasks = async () => {
   try {
     const { data } = await axios.get("/api/v1/tasks");
+    console.log(data.length);
+    if (data.length === 0) {
+      taskDom.innerHTML = `<h5 class="empty-list"> no To DO</h5>`;
+      return;
+    }
 
     const allTasks = data
       .map((task) => {
@@ -15,10 +21,10 @@ const showTasks = async () => {
         <span><i class="far fa-check-circle"></i></span>${name}
       </h5>
       <div class="task-links">
-        <a href="#" class="edit-link">
+        <a href="/edit.html" class="edit-link">
           <i class="fa-solid fa-pen-to-square"></i>
         </a>
-        <button href="#" class="delete-btn">
+        <button href="#" class="delete-btn" data-id="${_id}">
           <i class="fa-solid fa-trash-can"></i>
         </button>
       </div>
@@ -43,4 +49,19 @@ formDom.addEventListener("submit", async (event) => {
     console.log(err);
   }
 });
+
+taskDom.addEventListener("click", async (event) => {
+  const element = event.target;
+  if (element.parentElement.classList.contains("delete-btn")) {
+    const id = element.parentElement.dataset.id;
+
+    try {
+      await axios.delete(`/api/v1/tasks/${id}`);
+      showTasks();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
 showTasks();
